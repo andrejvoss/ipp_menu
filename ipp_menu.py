9e4d60be7aea191ee951dd0994bd2e3a089f6ff1
Jörg Encke <joerg.encke@tum.de>
2015-10-30 15:27:00 +0100

    Add GPL license files and header

--- ipp_menu.py
+++ ipp_menu.py
@@ -1,4 +1,23 @@
-#import sys
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Copyright (C) 2015  Jörg Encke
+This file is part of ipp_menu
+
+auditory_brain is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+auditory_brain is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ipp_menu.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
